db52477b8b2ee14dd6101444052878ca188cd209
Scott Barclay <75497242+csb1227@users.noreply.github.com>
2022-12-08 13:45:28 -0500

    part 2

--- 2022/Day 08/main.py
+++ 2022/Day 08/main.py
@@ -2,7 +2,9 @@ class Forest(object):
     def __init__(self, trees):
         self.trees = trees
         self._map_trees()
+        self._score_visibility()
         self.visible_trees = self._count_visible_trees()
+        self.best_tree = self._find_best_tree()
 
     def _map_trees(self):
         for coords, tree in self.trees.items():
@@ -58,6 +60,47 @@ class Forest(object):
 
         return visible_trees
 
+    def _score_visibility(self):
+        for _, tree in self.trees.items():
+            n_vis = 0
+            e_vis = 0
+            s_vis = 0
+            w_vis = 0
+
+            n_tree, e_tree, s_tree, w_tree = tree.north, tree.east, tree.south, tree.west
+
+            while n_tree:
+                n_vis += 1
+                if n_tree.height >= tree.height:
+                    break
+                n_tree = n_tree.north
+
+            while e_tree:
+                e_vis += 1
+                if e_tree.height >= tree.height:
+                    break
+                e_tree = e_tree.east
+
+            while s_tree:
+                s_vis += 1
+                if s_tree.height >= tree.height:
+                    break
+                s_tree = s_tree.south
+
+            while w_tree:
+                w_vis += 1
+                if w_tree.height >= tree.height:
+                    break
+                w_tree = w_tree.west
+
+            tree.visibility_score = n_vis * e_vis * s_vis * w_vis
+
+    def _find_best_tree(self):
+        best_tree = 0
+        for _, tree in self.trees.items():
+            best_tree = tree.visibility_score if tree.visibility_score > best_tree else best_tree
+        return best_tree
+
 
 class Tree(object):
     def __init__(self, height, coords):
@@ -67,6 +110,7 @@ class Tree(object):
         self.east = None
         self.south = None
         self.west = None
+        self.visibility_score = 0
 
 
 def build_forest(f):
@@ -93,3 +137,4 @@ if __name__ == '__main__':
     forest_raw = open_forest(puzzle_input)
     forest = build_forest(forest_raw)
     print(f'Part 1: {forest.visible_trees}')
+    print(f'Part 2: {forest.best_tree}')
