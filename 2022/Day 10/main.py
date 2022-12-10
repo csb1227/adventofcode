a3b674d94e16805765f241eb73bffe8827768277
Scott Barclay <75497242+csb1227@users.noreply.github.com>
2022-12-09 23:20:00 -0500

    ready to play

--- 2022/Day 10/main.py
+++ 2022/Day 10/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def open_puzzle(r):
+    with open(r, 'r') as f:
+        return f.read().split('\n')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    example_input = './input/example.txt'
+    puzzle_input = './input/puzzle.txt'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    puzzle = open_puzzle(example_input)
