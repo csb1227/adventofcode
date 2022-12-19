22d2d338189dc74d2c39d11a9ceaf720ecb2efb1
Scott Barclay <75497242+csb1227@users.noreply.github.com>
2022-12-19 14:40:46 -0500

    part 2

--- 2022/Day 12/main.py
+++ 2022/Day 12/main.py
@@ -14,12 +14,18 @@ def find_start_end(x):
     return start, end
 
 
+def find_all_starts(x):
+    starts = []
+    for ix_row, row in enumerate(x):
+        for ix_col, col in enumerate(row):
+            if x[ix_row][ix_col] == 97:
+                starts.append((ix_row, ix_col))
+
+    return starts
+
+
 def parse_input(x):
     return [[ord(point) for point in row] for row in open(x).read().split('\n')]
-
-
-def manhattan_distance(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
 def a_star_priority_queue(graph, start, end):
@@ -38,7 +44,9 @@ def a_star_priority_queue(graph, start, end):
         position = open_list.get()[1]
 
         # you got to the end
+        made_it = False
         if position == end:
+            made_it = True
             break
 
         try:
@@ -46,21 +54,31 @@ def a_star_priority_queue(graph, start, end):
             for offset in neighbor_offsets:
                 new_position = (position[0] + offset[0], position[1] + offset[1])
                 # if the new position is on the map
-                if 0 <= new_position[0] < len(graph) and 0 <= new_position[1] < len(graph[0]):
+                if 0 <= new_position[0] < len(graph) and 0 <= new_position[1] < len(graph[0]) and graph[new_position[0]][new_position[1]] <= graph[position[0]][position[1]] + 1:
                     # calculate the distance for going to the new position
                     new_distance = distance_so_far[position] + 1
                     # if the new position is not black listed and you can reach it
-                    if (new_position not in closed_list and graph[new_position[0]][new_position[1]] <= graph[position[0]][position[1]] + 1) or \
+                    if (new_position not in closed_list) or \
                             (new_position in distance_so_far.keys() and new_distance < distance_so_far[new_position]):
                         distance_so_far[new_position] = distance_so_far[position] + 1
-                        priority = new_distance # + manhattan_distance(new_position, end)
+                        priority = new_distance
                         open_list.put((priority, new_position))
                         closed_list[new_position] = position
 
         except IndexError:
             print(f'Error at: {new_position}')
 
-    return distance_so_far[position]
+    return distance_so_far[position], made_it
+
+
+def find_best_path(data, starts, end):
+    routes = PriorityQueue()
+    for start in starts:
+        route, made_id = a_star_priority_queue(data, start, end)
+        if made_id:
+            routes.put((route, start))
+
+    return routes.get()[0]
 
 
 if __name__ == '__main__':
@@ -73,11 +91,9 @@ if __name__ == '__main__':
     data[start[0]][start[1]] = ord('a')
     data[end[0]][end[1]] = ord('z')
 
-    path = a_star_priority_queue(data, start, end)
-
+    path, made_it = a_star_priority_queue(data, start, end)
     print(f'Part 1: {path}')
 
-# 564 too high
-# 538 too high
-# 526 too low
-
+    starts = find_all_starts(data)
+    best_path = find_best_path(data, starts, end)
+    print(f'Part 2: {best_path}')
