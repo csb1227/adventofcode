6daf7c0b53b39d6757d307903b08edb62849df4a
Scott Barclay <scott.barclay@ncontracts.com>
2024-12-04 14:04:29 -0500

    part 2

--- 2024/day04/day04methods.py
+++ 2024/day04/day04methods.py
@@ -1,3 +1,6 @@
+import re
+
+
 def search_rows(crossword: list[str], pattern: str) -> int:
     _pattern_occurrences = 0
     for row in crossword:
@@ -57,3 +60,30 @@ def search_backwards_diagonal(crossword: list[str], pattern: str) -> int:
     _pattern_occurrences = search_forward_diagonal(_mirrored_crossword, pattern)
 
     return _pattern_occurrences
+
+
+def create_groups(crossword: list[str], group_size: int) -> (list[list[str]], int):
+    _row_groups = []
+
+    for i in range(len(crossword) - (group_size - 1)):
+        _row_groups.append("".join(crossword[i:i + group_size]))
+
+    return _row_groups, len(crossword[0])
+
+def count_x_mas(row_groups: list[str], row_length: int) -> int:
+    _top_m_pattern = f"(?=(M[A-Z]M[A-Z]{{{row_length - 2}}}A[A-Z]{{{row_length - 2}}}S[A-Z]S))"
+    _right_m_pattern = f"(?=(S[A-Z]M[A-Z]{{{row_length - 2}}}A[A-Z]{{{row_length - 2}}}S[A-Z]M))"
+    _bottom_m_pattern = f"(?=(S[A-Z]S[A-Z]{{{row_length - 2}}}A[A-Z]{{{row_length - 2}}}M[A-Z]M))"
+    _left_m_pattern = f"(?=(M[A-Z]S[A-Z]{{{row_length - 2}}}A[A-Z]{{{row_length - 2}}}M[A-Z]S))"
+
+    _matches = []
+
+    for group in row_groups:
+        _matches.append(re.findall(_top_m_pattern, group))
+        _matches.append(re.findall(_right_m_pattern, group))
+        _matches.append(re.findall(_bottom_m_pattern, group))
+        _matches.append(re.findall(_left_m_pattern, group))
+
+    return sum(len(match) for match in _matches)
+
+
