617b6a7426e666aa257d0a5eb935549972720b0b
csb1227 <csb1227@gmail.com>
2025-12-06 23:35:28 -0500

    part 2

---
+++ 2025/day02/part02.py
@@ -0,0 +1,35 @@
+import re
+
+from input_reader import read_input
+
+valid_ranges = read_input("input.txt")
+
+invalids = set()
+
+def do_regex_stuff(length, number):
+    pattern = rf"(^\d{{{length}}})\1+"
+    match = re.fullmatch(pattern, str(number))
+    if match:
+        return True
+    else:
+        return False
+
+
+for begin, end in valid_ranges:
+    begin_int = int(begin)
+    end_int = int(end)
+
+    for x in range(begin_int, end_int + 1):
+        if x in invalids:
+            continue
+
+        len_x = len(str(x))
+
+        for i in range(1, 16):
+            if x == 111:
+                pass
+            if len_x % i == 0:
+                if do_regex_stuff(i, x):
+                    invalids.add(x)
+
+print(sum(invalids))
