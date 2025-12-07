919a8517afba34e1849034cde7f8d99bc0622c50
csb1227 <csb1227@gmail.com>
2025-12-06 20:27:34 -0500

    part 1

--- 2025/day02/part01.py
+++ 2025/day02/part01.py
@@ -1,6 +1,6 @@
 from input_reader import read_input, eliminate_impossible_ranges, eliminate_impossible_parts_of_ranges
 
-valid_ranges = read_input("example.txt")
+valid_ranges = read_input("input.txt")
 
 possible_ranges = eliminate_impossible_ranges(valid_ranges)
 
@@ -10,28 +10,21 @@ possible_ranges = eliminate_impossible_parts_of_ranges(possible_ranges)
 
 print(possible_ranges)
 invalids = set()
-for rng in possible_ranges:
-    begin = rng[0]
-    end = rng[1]
-    num_len = len(begin)
-    repeat_len = int(num_len / 2)
 
-    first_repeatable = begin[:repeat_len]
+for begin, end in possible_ranges:
+    repeat_len = len(begin) // 2
 
-    half_max_begin = max(int(begin[:repeat_len]), int(begin[repeat_len:]))
-    half_min_end = min(int(end[:repeat_len]), int(end[repeat_len:]))
+    half_begin = int(begin[:repeat_len])
+    half_end = int(end[:repeat_len])
 
-    max_begin = int(f"{half_max_begin}{half_max_begin}")
-    min_end = int(f"{half_min_end}{half_min_end}")
+    begin_int, end_int = int(begin), int(end)
 
-    if int(begin) <= max_begin <= int(end):
-        invalids.add(max_begin)
-
-    if int(begin) <= min_end <= int(end):
-        invalids.add(min_end)
-
-
+    for x in range(half_begin, half_end + 1):
+        y = int(f"{x}{x}")
+        if begin_int <= y <= end_int:
+            invalids.add(y)
 
 print(sum(invalids))
 
 # 22380332142 too low
+# 30608905813
